88b65d94fefe47ea4145c5e6735fcab3dbb12f1d
Andrei Panferov <andrei@panferov.org>
2025-10-17 12:00:47 +0200

    backward works

--- inference_lib/src/fp_quant/utils/__init__.py
+++ inference_lib/src/fp_quant/utils/__init__.py
@@ -1,2 +1,2 @@
 from .config import FPQuantConfig, FPQuantDtype, validate_config
-from .replace import replace_with_fp_quant_linear
+from .replace import replace_with_fp_quant_linear, replace_quantize_with_fp_quant_linear
